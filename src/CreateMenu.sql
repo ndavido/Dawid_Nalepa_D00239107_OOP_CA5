7f4f24b1b7df003a2f942ddccd8cb080a230511e
ndavido <D00239107@student.dkit.ie>
2022-04-19 21:14:45 +0100

    Changes to sql data

--- src/CreateMenu.sql
+++ src/CreateMenu.sql
@@ -3,21 +3,23 @@ CREATE DATABASE `oop_ca4`;
 USE `oop_ca4`;
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
-                        `MENU_ID` int(11) NOT NULL AUTO_INCREMENT,
+                        `MENU_ID` int(11) NOT NULL,
                         `NAME` varchar(50) NOT NULL,
                         `DISH_SIZE` varchar(50) NOT NULL,
                         `QUANTITY` INT(20) NOT NULL,
-                        `PRICE` DOUBLE (20,2) NOT NULL,
-                        PRIMARY KEY  (`MENU_ID`)
-);
+                        `PRICE` DOUBLE (20,2) NOT NULL
+                        )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO menu VALUES (null, "Mushroom Soup", "Medium",1, 15.99),
-                        (null, "Spaghetti","Small",2,18.50),
-                        (null, "Mashed Potatoes","Large",1,20.99),
-                        (null, "Cranberry Sorbet","Medium",1,9.99),
-                        (null, "Warm Ice-Cream","Large",3,14.99),
-                        (null, "Lasagna","Small",1,17.99),
-                        (null, "Caesar Salad","Small",1,8.99),
-                        (null, "Chicken Tika Masala","Medium",1,15.99),
-                        (null, "Smoked Salmon","Small",1,25.99),
-                        (null,"Steak","Large",1,59.99);
+INSERT INTO menu VALUES (1, "Mushroom Soup", "Medium",1, 15.99),
+                        (2, "Spaghetti","Small",2,18.50),
+                        (3, "Mashed Potatoes","Large",1,20.99),
+                        (4, "Cranberry Sorbet","Medium",1,9.99),
+                        (5, "Warm Ice-Cream","Large",3,14.99),
+                        (6, "Lasagna","Small",1,17.99),
+                        (7, "Caesar Salad","Small",1,8.99),
+                        (8, "Chicken Tika Masala","Medium",1,15.99),
+                        (9, "Smoked Salmon","Small",1,25.99),
+                        (10,"Steak","Large",1,59.99);
+
+ALTER TABLE `menu` ADD PRIMARY KEY (`MENU_ID`);
+ALTER TABLE `menu` MODIFY `MENU_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
